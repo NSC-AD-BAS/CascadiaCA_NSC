d6a06776174b6815d2267c132f46531d80b7369a
SamuelioX <samueliox@gmail.com>
2016-05-03 20:47:22 -0700

    added new getEvent.php that echos oout the array

--- plugin/php/getEvent.php
+++ plugin/php/getEvent.php
@@ -1,23 +1,24 @@
 <?php
-	/**
-	 * function that returns the events as $json data
-	 */
-	 
-	include "connect.php";
-	//$startDate, $endDate, $eventTypes, $topics
-	public function getEvents($startDate, $endDate){
-		header('Content-type: application/javascript');
-		$db = connect();
-		//fetch table rows from mysql db
-		$sql = "select * from event between dateStart and dateEnd";
-		$result = mysqli_query($db, $sql) or die("Error in Selecting " . mysqli_error($db));
-		$eventarray = array();
-		while ($row = mysqli_fetch_assoc($result)) {
-			$eventarray[] = $row;
-		}
-		$jsonarray = json_encode($eventarray);
-		//close the db connection
-		mysqli_close($db);
-		return $jsonarray;
-	}
+
+/**
+ * function that returns the events as $json data
+ */
+function getEvent() {
+    include "connect.php";
+//$startDate, $endDate, $eventTypes, $topics
+    header('Content-type: application/javascript');
+    $db = connect();
+//fetch table rows from mysql db
+    $sql = "select * from event";
+    $result = mysqli_query($db, $sql) or die("Error in Selecting " . mysqli_error($db));
+    $eventarray = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $eventarray[] = $row;
+    }
+    $jsonarray = json_encode($eventarray);
+//close the db connection
+    mysqli_close($db);
+    return $jsonarray;
+}
+echo getEvent();
 ?>
