7154f22d07a0394b364bfb0cbaca288a4e9c3191
SamuelioX <samueliox@gmail.com>
2016-05-07 21:50:17 -0700

    "submitting a form will insert everything into a database, currently no duplicate testing."

--- plugin/php/create_event_form.php
+++ plugin/php/create_event_form.php
@@ -15,7 +15,8 @@
             <!-- calendar input for date-->
             Start Date*:<input type="date" name="start_date" required value="mm/dd/yyyy">
             End Date*:<input type="date" name="end_date" required value="mm/dd/yyyy">
-            Start Time*:<input type="time" name="start_time" value="--:-- --"><br>
+            Start Time*:<input type="time" name="start_time" value="--:-- --">
+            End Time*:<input type="time" name="end_time" value="--:-- --"><br>
             Building*:<input type="text" name="building" required><br>
             Address*:<input type="text" name="street_address" required><br>
             City*:<input type="text" name="city" required><br>
@@ -23,12 +24,13 @@
             Zip*:<input type="text" name="zip" required><br>
             
             Organization<br>
-            Organization Name*:<input type="text" name="organization_name"><br>
-            Organization Website:<input type="text" name="organization_website"><br>
-            Organization Address:<input type="text" name="organization_address"><br>
-            Organization City:<input type="text" name="organization_city"><br>
-            Organization State:<input type="text" name="organization_state"><br>
-            Organization Zip:<input type="text" name="organization_zip"><br>
+            Organization Name*:<input type="text" name="organization_name" required><br>
+            Organization Website*:<input type="text" name="organization_website" required><br>
+            Organization Building*:<input type="text" name="organization_building" required><br>
+            Organization Address*:<input type="text" name="organization_address" required><br>
+            Organization City*:<input type="text" name="organization_city" required><br>
+            Organization State*:<input type="text" name="organization_state" required><br>
+            Organization Zip*:<input type="text" name="organization_zip" required><br>
 
             Contact Info<br>
             First Name*:<input type="text" name="first_name" required><br>
@@ -37,7 +39,7 @@
             Phone Number*:<input type="text" name="phone_number" required><br>
                 Select image to upload:
                 <input type="file" name="fileToUpload" id="fileToUpload"><br>
-                <input type="submit" value="Submit New Event">
+                <input type="submit" name="submit" value="Submit New Event">
             </form>
 
 
