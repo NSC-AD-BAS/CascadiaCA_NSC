fc2ebc5ffb01dfc0b5be5b3cd62881a3829bfa92
transientcw <chris@myubercode.com>
2016-05-28 22:38:45 -0700

    still testing

--- TEST/JS/Launcher.js
+++ TEST/JS/Launcher.js
@@ -7,89 +7,77 @@ var dom, S, M, currentArray, buttonArray, typesArray, topicsArray;
             allContent: {},
             urlList: {
                 allURL: "../PHP/getEventsListView.php",
-                typesURL: "../PHP/getTopics.php",
-                topicsURL: "../PHP/getTypes.php",
+                typesURL: "../PHP/getTypes.php",
+                topicsURL: "../PHP/getTopics.php",
                 filterURL: ""
             },
             eventObjArray: [],
             buttonObjArray: [],
-            typesArray: {},
-            topicsArray: {}
+            eventTypesArray: [],
+            eventTopicsArray: []
         },
 
         methods: {
-            allCallBack: function(j) {
+            allCallBack: function (j) {
                 // Issues here: adding any string such as "test" to an array of objects automatically converts the objects
                 //  into strings, so we need to build all Event objects first then we can test them.
                 S.allContent = j;
                 var list = JSON.parse(S.allContent);
-                for(var index in list) {
+                for (var index in list) {
                     var event = new Event(list[index]);
                     S.eventObjArray.push(event);
                 }
             },
-            ajax: function(url, callback) {
+            topicsCallBack: function(j) {
+                S.eventTopicsArray = j;
+                var list = JSON.parse(S.eventTopicsArray);
+                M.populateTopicsDropDown(list);
+            },
+            typesCallBack: function(j) {
+                S.eventTypesArray = j;
+                var list = JSON.parse(S.eventTypesArray);
+                M.populateTypesDropDown(list);
+            },
+            ajax: function (url, callback) {
                 getAjax(url, callback);
             },
-            testList: function(listIn) {
+            testList: function (listIn) {
                 console.log("inside testList method, do we have a global variable?");
                 console.log(currentArray);
             },
-            setAllEvents: function(listIn) {                
+            setAllEvents: function (listIn) {
                 currentArray = listIn;
             },
-            getAllEvents: function() {
+            getAllEvents: function () {
                 return currentArray;
             },
-            setAllButtons: function(listIn) {
+            setAllButtons: function (listIn) {
                 buttonArray = listIn;
             },
-            getAllButtons: function() {
+            getAllButtons: function () {
                 return buttonArray;
             },
-            setEventNavigation: function() {
-                console.log("called nav function");
-                var length = currentArray.length;
-                console.log("length " + length);
-                var counter = 0;
-                var endIndex = length - 1;
-                if(currentArray.length < 4) {
-                    switch(length) {
-                        case 2:
-                            var first = currentArray[0];
-                            var second = currentArray[1];
-                            first.setNextEvent(second);
-                            second.setPreviousEvent(first);
-                            break;
-                        case 3:
-                            var first = currentArray[0];
-                            var second = currentArray[1];
-                            var third = currentArray[2];
-                            first.setNextEvent(second);
-                            second.setPreviousEvent(first);
-                            second.setNextEvent(third);
-                            third.setPreviousEvent(second);
-                            break;
-                        default:
-                            break;
-                    }
+            populateTopicsDropDown: function(listTopics) {
+                console.log("inside topics drop");
+                console.log(listTopics);
+                var topicsDropDown = document.getElementById("topics");
+                for(var index in listTopics) {
+                    var op = document.createElement("option");
+                    op.setAttribute("id", "dd" + index);
+                    op.innerHTML = listTopics[index].Main_Topic;
+                    topicsDropDown.appendChild(op);
                 }
-                else {
-                    console.log("entering longer navigation case");
-                    var firstE = currentArray[0];
-                    console.log(firstE);
-                    var lastE = currentArray[endIndex];
-                    firstE.setNextEvent(currentArray[1]);
-                    lastE.setPreviousEvent(currentArray[endIndex - 1]);
-                    counter++;
-                    while(counter > 0 && counter < endIndex) {
-                        var current = currentArray[counter];
-                        current.setPreviousEvent(currentArray[counter - 1]);
-                        current.setNextEvent(currentArray[counter + 1]);
-                        counter++;
-                    }
+            },
+            populateTypesDropDown: function(listTypes) {
+                console.log("inside types drodown");
+                console.log(listTypes);
+                var typesDropDown = document.getElementById("types");
+                for(var index2 in listTypes) {
+                    var op2 = document.createElement("option");
+                    op2.setAttribute("id", "dd2" + index2);
+                    op2.innerHTML = listTypes[index2].Main_Type;
+                    typesDropDown.appendChild(op2);
                 }
-
             }
         },
 
@@ -99,13 +87,17 @@ var dom, S, M, currentArray, buttonArray, typesArray, topicsArray;
             M = this.methods;
             currentArray = S.eventObjArray;
             buttonArray = S.buttonObjArray;
+            topicsArray = S.eventTopicsArray;
+            typesArray = S.eventTypesArray;
             M.ajax(S.urlList.allURL, M.allCallBack);
             var tempList = S.eventObjArray;
             M.setAllEvents(tempList);
             M.testList(tempList);
-            M.setEventNavigation();
+            //M.setEventNavigation();
+            M.ajax(S.urlList.topicsURL, M.topicsCallBack);
+            M.ajax(S.urlList.typesURL, M.typesCallBack);
         }
-    }
+    };
 
 
 
