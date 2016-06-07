d4df41ddede09a98e6b13f99d48186df7953d94c
transientcw <chris@myubercode.com>
2016-06-06 17:33:06 -0700

    added TODOs

--- TEST/js/Filter.js
+++ TEST/js/Filter.js
@@ -2,6 +2,7 @@
  * Created by cdub on 6/4/2016.
  */
 var calShowing = false;
+var dateTimeFromForm;
 
 function swapCalendar(calBtn) {
     calBtn.addEventListener('click', function(){
@@ -28,19 +29,28 @@ function resetDates(btn) {
 }
 
 function resetAll(btn) {
+    // TODO: clear all values from all drop down lists
     btn.addEventListener('click', function(){
         console.log('reset');
     }, false)
 }
 
 function search(btn) {
+    // TODO: get all values from the drop down lists, and dateTimes, add to an array, and process an ajax request
     btn.addEventListener('click', function(){
         console.log('search filters');
+        //var type = document.getElementById("")
     }, false)
 }
 
 function createNew(btn) {
     btn.addEventListener('click', function(){
+        // add functionality to tie in with duri and sam's form submission
         console.log('create new');
-    }, false)
+}, false)
+}
+
+function dateTimeSaver(dateTime) {
+    dateTimeFromForm = dateTime;
+    // TODO: store this for putting into our array of filter items selected
 }
